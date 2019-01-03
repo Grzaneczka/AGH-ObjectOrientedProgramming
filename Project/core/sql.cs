f82363f0b6f7ef7bef05cbf6c4f7080ec433fc49
Szymon Bednarek <behemkotel@gmail.com>
2019-01-03 20:36:08 +0100

    Adding sql class

---
+++ Project/core/sql.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.core
+{
+    class Sql
+    {
+        public Sql()
+        {
+
+        }
+
+
+        void addTable()
+        {
+
+        }
+
+
+    }
+}
